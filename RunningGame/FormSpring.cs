959cf81edff76c094852818cb0e47e0b9b46fe66
Lina <lh3su@virginia.edu>
2014-02-27 12:08:00 -0500

    World 3 Level 1 added

--- RunningGame/FormSpring.cs
+++ RunningGame/FormSpring.cs
@@ -176,7 +176,7 @@ namespace RunningGame {
             GlobalVars.levels[1][1] = "RunningGame.Resources.Levels.World2Level2.png";
             GlobalVars.levels[1][2] = "RunningGame.Resources.Levels.World2Level31.png";
 
-            GlobalVars.levels[2][0] = "RunningGame.Resources.Levels.World3Level1.png";
+            GlobalVars.levels[2][0] = "RunningGame.Resources.Levels.World3Level1_new.png";
             GlobalVars.levels[2][1] = "RunningGame.Resources.Levels.World3Level2.png";
             GlobalVars.levels[2][2] = "RunningGame.Resources.Levels.World3Level3.png";
 
