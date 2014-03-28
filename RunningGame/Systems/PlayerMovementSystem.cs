83ade92a69bb7e06d18a6e57b3cff81ba04a021c
akg4jq <annagrn101@gmail.com>
2014-03-28 15:02:43 -0400

    2-1

--- RunningGame/Systems/PlayerMovementSystem.cs
+++ RunningGame/Systems/PlayerMovementSystem.cs
@@ -57,7 +57,7 @@ namespace RunningGame.Systems {
 
                 //If there's a key down and the player isn't moving horizontally, check to make sure there's a collision
                 restartHorizontalMovementIfNoBlock( velComp, posComp, pelInComp, animComp );
-                if ( level != null ) {
+                if ( level != null && level.getPlayer() != null ) {
                     if ( level.getInputSystem().myKeys[GlobalVars.KEY_RIGHT].pressed || level.getInputSystem().myKeys[GlobalVars.KEY_LEFT].pressed ) {
                         level.getPlayer().startAnimation();
                     } else {
