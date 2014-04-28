d84db99207912be442dec6736810eba706619c20
Thomas Sparks <tws2xa@virginia.edu>
2014-04-28 03:54:06 -0400

    World 4 Level 3

--- RunningGame/FormSpring.cs
+++ RunningGame/FormSpring.cs
@@ -263,7 +263,7 @@ namespace RunningGame
 
             GlobalVars.levels[3][0] = "RunningGame.Resources.Levels.World4Level1_new.png";
             GlobalVars.levels[3][1] = "RunningGame.Resources.Levels.World4Level2_new.png";
-            GlobalVars.levels[3][2] = "RunningGame.Resources.Levels.World4Level31.png";
+            GlobalVars.levels[3][2] = "RunningGame.Resources.Levels.World4Level3_new.png";
 
             GlobalVars.levels[4][0] = "RunningGame.Resources.Levels.World5Level1_new.png";
             GlobalVars.levels[4][1] = "RunningGame.Resources.Levels.World5Level2_new.png";
